dcf5afb2167cc28471275d1200b13ef513bdb8ac
elenapetra <elena_petrasco@epam.com>
2024-03-22 20:21:47 +0200

    Fixed importFileParser function and added swagger file

--- import-service/importFileParser.js
+++ import-service/importFileParser.js
@@ -4,23 +4,35 @@ const s3 = new AWS.S3({ region: "eu-west-1" });
 const bucketName = "import-products-data-bucket";
 
 module.exports.importFileParser = async (event) => {
-  event.Records.forEach((record) => {
-    const key = record.s3.object.key;
+  try {
+    await Promise.all(
+      event.Records.map(async (record) => {
+        const key = record.s3.object.key;
+        const s3Stream = s3
+          .getObject({
+            Bucket: bucketName,
+            Key: key,
+          })
+          .createReadStream();
 
-    const s3Stream = s3
-      .getObject({
-        Bucket: bucketName,
-        Key: key,
+        await new Promise((resolve, reject) => {
+          s3Stream
+            .pipe(csv())
+            .on("data", (data) => {
+              console.log("Parsed record:", data);
+            })
+            .on("end", () => {
+              console.log("Copy from " + bucketName + "/" + key);
+              resolve();
+            })
+            .on("error", (error) => {
+              console.error("Error processing file:", key, error);
+              reject(error);
+            });
+        });
       })
-      .createReadStream();
-
-    s3Stream
-      .pipe(csv())
-      .on("data", (data) => {
-        console.log("Parsed record:", data);
-      })
-      .on("end", async () => {
-        console.log("Copy from " + bucketName + "/" + key);
-      });
-  });
+    );
+  } catch (error) {
+    console.error("Error processing S3 event:", error);
+  }
 };
